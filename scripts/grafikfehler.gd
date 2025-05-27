8bfdbb5e9ab8c48656ad04453362def11fe89732
julixus <ju@weisscc.de>
2025-05-27 16:41:10 +0200

    Grafikfehler Bilder gemerged

--- scripts/grafikfehler.gd
+++ scripts/grafikfehler.gd
@@ -8,21 +8,21 @@ var punkte : int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	check_button.position = Vector2(100, 100)
+	check_button.position = Vector2(-60, -225)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	label.text = "Punkte: " + str(punkte)
 	if Input.is_action_just_pressed("TurnLeft") || punkte == 1:
-		sprite_2d.texture = ResourceLoader.load("C:/Users/olche/Downloads/kohmi_games_v2/Picture/Test1.png")
-		check_button.position = Vector2(100, 150)
+		sprite_2d.texture = ResourceLoader.load("res://res/Grafikfehler1.jpg")
+		check_button.position = Vector2(90, -65)
 	if Input.is_action_just_pressed("TurnRight") || punkte == 2:
-		sprite_2d.texture = ResourceLoader.load("C:/Users/olche/Downloads/kohmi_games_v2/Picture/Test2.png")
-		check_button.position = Vector2(200, 100)
+		sprite_2d.texture = ResourceLoader.load("res://res/Grafikfehler2.jpg")
+		check_button.position = Vector2(180, -130)
 	if Input.is_action_just_pressed("Move") || punkte == 3:
-		sprite_2d.texture = ResourceLoader.load("C:/Users/olche/Downloads/kohmi_games_v2/Picture/Test3.png")
-		check_button.position = Vector2(100, 100)
+		sprite_2d.texture = ResourceLoader.load("res://res/Grafikfehler3.jpg")
+		check_button.position = Vector2(-60, -225)
 
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
